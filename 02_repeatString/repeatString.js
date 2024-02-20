05d1906c7e291a8a2d6dcec37398704c0842a7d2
jseng01 <jseng777@gmail.com>
2024-02-19 19:23:34 -0800

    Complete all tests for repeat string excersize

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,14 @@
 let string = 'hey';
 let number = 0;
 function repeatString(string, number){    
-    let newString = '';
-    for (let i = 1; i <= number; i++){
-    newString += string;
+    if (number >= 0){
+        let newString = '';
+        for (let i = 1; i <= number; i++){
+        newString += string;
 }
 return newString
+    }
+    else return 'ERROR';
 }
 // Do not edit below this line
 module.exports = repeatString;
