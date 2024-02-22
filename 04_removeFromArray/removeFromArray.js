ad8368f4bc158ff3b3fa3abf9a52a735d07e52f7
jseng01 <jseng777@gmail.com>
2024-02-22 12:01:51 -0500

    Complete first test condition

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args ) { 
+for (const item of array) {
+    if (item == args){
+     let position = array.indexOf(item);
+     array.splice(position, 1)
+}
 
-};
+   else{
+     continue;
+   }
+}
+return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
