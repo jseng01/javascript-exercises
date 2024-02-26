c070511e012db07621d966373baee1d4a3c23869
jseng01 <jseng777@gmail.com>
2024-02-26 01:53:44 -0500

    Pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,30 @@
 const sumAll = function() {
 let total = 0;
 let toAdd = [];
-for (let i = arguments[0]; i <= arguments[1]; i++){
-    toAdd.push(i);
+
+if (arguments[0] < 0 || arguments[1] < 0){
+    return "ERROR";
 }
-toAdd.forEach(num =>{
-    total += num;
-})
+if (!Number.isInteger(arguments[0]) || !Number.isInteger(arguments[1])){
+    return "ERROR"}
+else if (arguments[0] < arguments[1]){
+        for (let i = arguments[0]; i <= arguments[1]; i++){
+            toAdd.push(i);
+            }
+        }
+    else if(arguments[0] > arguments[1]){
+        for (let i = arguments[1]; i <= arguments[0]; i++){
+            toAdd.push(i);
+            }
+        } 
+
+
+   
+    toAdd.forEach(num =>{
+        total += num;
+        })
 return total;
-};
+    }
 // establish a counter set to the first argument
 // have the counter add itself to a variable before iterating
 // iterate until the counter reaches the second argument
